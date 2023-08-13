d9424395b08bd1e0f491fd8946053c22e60eb593
ayush4345 <99096397+ayush4345@users.noreply.github.com>
2023-08-13 15:03:33 +0530

    added button

---
+++ components/TokenGating.jsx
@@ -0,0 +1,7 @@
+export default function TokenGating({id}){
+    return (
+        <main className="h-full w-full flex justify-center items-center">
+            <button className="relative rounded-xl text-white text-lg flex items-center bg-black py-3 px-5">Verify</button>
+        </main>
+    )
+}
